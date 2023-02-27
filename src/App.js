7ae82af923cafc178a18a638ebbbb9abca4d1db5
Corey N <coreynotice@yahoo.com>
2023-02-27 12:11:22 -0500

    bootstap with react intro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+
+
 
 
 
 function App() {
   return (
+    
     <div className="App">
-      <h1>Bootstrap Website</h1>
-     
+      <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Corey's Nav</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">To the Moon</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">And Beyond</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Blahhh...</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      <h1>Corey N's Bootstrap Website</h1>
+      
+    <>
+    <Button variant="outline-success">Success</Button>{' '}
+      </>
+      
     </div>
   );
 }
